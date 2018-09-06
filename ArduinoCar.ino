832a4729db3c1474b2fcc8f11c09439071a9a3e4
Niraj-kumar99 <36122263+Niraj-kumar99@users.noreply.github.com>
2018-09-06 23:02:21 +0530

    Create ArduinoCar.ino

---
+++ ArduinoCar.ino
@@ -0,0 +1,75 @@
+char command;
+void setup() 
+{       
+  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+}
+
+void loop(){
+  if(Serial.available() > 0){ 
+    command = Serial.read(); 
+   // Stop(); //initialize with motors stoped
+    //Change pin mode only if new command is different from previous.   
+    //Serial.println(command);
+    switch(command){
+    case 'F':  
+      forward();
+      break;
+    case 'B':  
+       back();
+      break;
+    case 'L':  
+      left();
+      break;
+    case 'R':
+      right();
+      break;
+    case 'S':
+      Stop();
+      break;
+    }
+  } 
+}
+
+void forward()
+{
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+}
+
+void back()
+{
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+}
+
+void left()
+{
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,LOW);
+}
+
+void right()
+{
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,HIGH);
+}
+
+void Stop()
+{
+   digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(7,LOW);
+}
